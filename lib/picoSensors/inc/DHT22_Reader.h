749756df23499cb5efa4c0f08b8796f4a1615789
Amancio Gómez Vernet <amanciogomezvernet@gmail.com>
2024-06-15 00:30:05 +0200

    added picoSensors lib

---
+++ lib/picoSensors/inc/DHT22_Reader.h
@@ -0,0 +1,85 @@
+/**
+ * @file DHT22_Reader.h
+ * @brief DHT22 Sensor Reader
+ *
+ * @author Amancio Gomez Vernet
+ * @license GNU General Public License v3.0
+ *
+ * @details
+ * This software is free to use, modify, and distribute under the terms of the
+ * GNU General Public License v3.0. See
+ * https://www.gnu.org/licenses/gpl-3.0.html for details.
+ *
+ * This code was heavily inspired by the implementation found at:
+ * https://github.com/gosouth/DHT22/blob/master/main/DHT22.c
+ */
+
+#pragma once
+
+#define DHT22READERS_MAX 4
+#define MSG_BYTE_SIZE 5
+
+typedef struct DHT22Reader {
+  int pin;
+  float *temperature;
+  float *humidity;
+} DHT22Reader;
+
+/**
+ * @brief Initializes a DHT22 sensor reader with the specified pin and data
+ * pointers.
+ *
+ * This function initializes a DHT22 sensor reader with the specified GPIO pin
+ * number and pointers to store the temperature and humidity readings. It
+ * assigns these values to the internal data structure `DHT22Readers`. If the
+ * initialization is successful, it returns the index of the initialized reader.
+ * If no more readers can be initialized, it returns -1.
+ *
+ * @param[in] pin The GPIO pin number to which the DHT22 sensor is connected.
+ * @param[out] temperature Pointer to a float variable to store the temperature
+ * reading.
+ * @param[out] humidity Pointer to a float variable to store the humidity
+ * reading.
+ * @return Index of the initialized DHT22 reader in the `DHT22Readers` array, or
+ * -1 if no more readers can be initialized.
+ */
+int DHT22_init(int pin, float *temperature, float *humidity);
+
+/**
+ * @brief Measures the duration a GPIO pin stays at a specified state.
+ *
+ * This function measures the time (in microseconds) for which a specified
+ * GPIO pin remains at a given state (high or low). The measurement is
+ * performed using a busy-wait loop, and the function will return after
+ * the pin changes state or the timeout is reached.
+ *
+ * @param[in] id of the DHT22 reader to read from.
+ * @param[in] state The GPIO state to measure (true for high, false for low).
+ * @param[in] timeout The maximum time to wait (in microseconds) for the pin
+ * to change state.
+ *
+ * @return The duration (in microseconds) the pin stayed at the specified state,
+ *         or -1 if the timeout was reached.
+ */
+int _getSignalLevel(int id, int state, int timeout);
+
+/**
+ * @brief Reads data from DHT22 sensor and writes into temp and humidity
+ * pointers.
+ *
+ * This function reads data from a DHT22 sensor, consisting of 16 bits humidity,
+ * 16 bits temperature, and an 8-bit checksum. It converts the received binary
+ * data into decimal values for temperature and humidity, and checks the
+ * checksum to ensure data integrity before writing the results to the specified
+ * memory locations pointed to by temp and humidity.
+ *
+ * @param[out] temp Pointer to a float variable where the temperature value (in
+ * Celsius) will be stored.
+ * @param[out] humidity Pointer to a float variable where the humidity value (in
+ * percentage) will be stored.
+ * @return Returns 0 on successful read and checksum verification, or -1 if an
+ * error occurred (checksum mismatch).
+ *
+ */
+
+int DHT22_read(int id);
---
+++ lib/picoSensors/src/DHT22_Reader.c
@@ -0,0 +1,93 @@
+/**
+ * @file DHT22_Reader.h
+ * @brief DHT22 Sensor Reader
+ *
+ * @author Amancio Gomez Vernet
+ * @license GNU General Public License v3.0
+ *
+ * @details
+ * This software is free to use, modify, and distribute under the terms of the
+ * GNU General Public License v3.0. See
+ * https://www.gnu.org/licenses/gpl-3.0.html for details.
+ *
+ * This code was heavily inspired by the implementation found at:
+ * https://github.com/gosouth/DHT22/blob/master/main/DHT22.c
+ */
+
+#include "DHT22_Reader.h"
+#include "hardware/gpio.h"
+#include "pico/stdlib.h"
+#include "pico/time.h"
+
+// up to 4 sensors connected at once
+int count = 0;
+DHT22Reader DHT22Readers[DHT22READERS_MAX];
+
+int DHT22_init(int pin, float *temperature, float *humidity) {
+  if (DHT22Readers[count].pin == 0) {
+    DHT22Readers[count].pin = pin;
+
+    DHT22Readers[count].temperature = temperature;
+    *(DHT22Readers[count].temperature) = 0;
+
+    DHT22Readers[count].humidity = humidity;
+    *(DHT22Readers[count].humidity) = 0;
+
+    gpio_init(pin);
+    return count++;
+  }
+  return -1;
+}
+
+int _getSignalLevel(int id, int state, int timeout) {
+
+  uint64_t starting_time = time_us_64();
+
+  while (gpio_get(DHT22Readers[id].pin) == state) {
+    if ((int)(time_us_64() - starting_time) > timeout)
+      return -1;
+  }
+
+  return (int)(time_us_64() - starting_time);
+}
+
+int DHT22_read(int id) {
+  uint8_t data_buffer[MSG_BYTE_SIZE];
+
+  gpio_set_dir(DHT22Readers[0].pin, GPIO_OUT);
+
+  gpio_put(DHT22Readers[0].pin, 0);
+  sleep_us(3000);
+
+  gpio_put(DHT22Readers[0].pin, 1);
+  sleep_us(30);
+
+  gpio_set_dir(DHT22Readers[0].pin, GPIO_IN);
+
+  if (_getSignalLevel(id, 0, 85) < 0)
+    return -1;
+
+  if (_getSignalLevel(id, 1, 85) < 0)
+    return -1;
+
+  int usec;
+  for (int i = 0; i < MSG_BYTE_SIZE * 8; i++) {
+
+    if (_getSignalLevel(id, 0, 56) < 0)
+      return -1;
+
+    usec = _getSignalLevel(id, 1, 75);
+
+    if (usec < 0)
+      return -1;
+    else if (usec > 40)
+      data_buffer[i / 8] |= (1 << (7 - (i % 8)));
+  }
+
+  *(DHT22Readers[id].humidity) =
+      (float)((data_buffer[0] << 8) + data_buffer[1]) / 10;
+  *(DHT22Readers[id].temperature) =
+      (float)(((data_buffer[2] & 0x7F) << 8) + data_buffer[3]) / 10;
+
+  return 1;
+}
