13d3ca8e3d97a655802876632f0eb526d8918078
Amancio Gómez Vernet <amanciogomezvernet@gmail.com>
2024-06-15 00:31:29 +0200

    testing DHT22

--- src/main.c
+++ src/main.c
@@ -2,10 +2,15 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+#include "DHT22_Reader.h"
+
 #define GPIO_ON 1
 #define GPIO_OFF 0
 
 #define LED_PIN 0
+
+float temperature;
+float humidity;
 
 int main() {
   stdio_init_all();
@@ -16,11 +21,15 @@ int main() {
   }
 
   while (true) {
-    printf("Hello, Pico!\n");
     cyw43_arch_gpio_put(LED_PIN, GPIO_ON);
     sleep_ms(500);
     cyw43_arch_gpio_put(LED_PIN, GPIO_OFF);
     sleep_ms(500);
+
+    int id = DHT22_init(18, &temperature, &humidity);
+
+    DHT22_read(id);
+    printf("Temperature: %f, Humidity: %f\n", temperature, humidity);
   }
   return 0;
 }
